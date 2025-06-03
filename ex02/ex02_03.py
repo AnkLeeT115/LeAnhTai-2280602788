136b152f6df487f33acc4d6de853e9fa3fe990bc
AnkLeeT115 <lee.tai204@gmail.com>
2025-06-03 21:01:16 +0700

    Lab-01

---
+++ ex02/ex02_03.py
@@ -0,0 +1,6 @@
+so =  int(input("Nhap 1 so nguyên:"))
+
+if so % 2 == 0 :
+    print(so, "la số chẵn")
+else: 
+    print(so, "là số lẻ")
---
+++ ex02/ex02_06.py
@@ -0,0 +1,10 @@
+input_str =  input("NHAP x ,y:")
+dimesions = [int(x) for x in input_str.split(',')]
+
+rowNum  = dimesions[0]
+colNum = dimesions[1]
+multilist = [[0 for col in range(colNum)] for row in range(rowNum)]
+for row in range(rowNum):
+    for col in range(colNum):
+        multilist[row][col] = row*col
+print(multilist)
